363496a77c2b2a13dac1364f405fd1cf7b97d17b
Berzan <berzan@tutanota.com>
2024-02-10 20:00:24 +0000

    update: documentation website

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -22,6 +22,10 @@ export default defineConfig({
                     label: "Project Management",
                     link: "/management",
                 },
+                {
+                    label: "Client Methods",
+                    link: "/client",
+                },
             ],
         }),
     ],
